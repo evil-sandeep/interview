19d3a2bcd5d4f508dc2a1173933fdf4875c1ee72
Sandeep Kumar Sahoo <chandansandeep2000.com>
2025-12-17 12:39:19 +0530

    async and await example

---
+++ fileReadUsingAsyncAwait.js
@@ -0,0 +1,14 @@
+import { error } from 'console'
+import fs from 'fs/promises'
+
+const reedFile=async()=>{
+    try{
+         const data=await fs.readFile('data.txt','utf8')
+    
+    console.log('File read success', data)
+    }catch(e){
+console.log('An error occoured found ', e)
+    }
+   
+}
+export default reedFile
